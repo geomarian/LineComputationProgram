4ae2593572bd205f9128bccbad250cef5176e586
geomarian <geomarian520@gmail.com>
2022-09-07 22:26:59 +0530

    Compare length UC3

--- src/com/bridgelabz/CalculateLength.java
+++ src/com/bridgelabz/CalculateLength.java
@@ -3,6 +3,8 @@ package com.bridgelabz;
  * 
  */
 
+import java.util.Scanner;
+
 /**
  * @author ASUS
  *
@@ -13,31 +15,49 @@ public class CalculateLength {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int x1 = 6;
-		int y1 = 9;
-		int x2 = 10;
-		int y2 = 12;
+
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("enter X1 co-ordinate");
+		int x1 = sc.nextInt();
+		System.out.println("enter Y1 co-ordinate");
+		int y1 = sc.nextInt();
+		System.out.println("enter X2 co-ordinate");
+		int x2 = sc.nextInt();
+		System.out.println("enter Y2 co-ordinate");
+		int y2 = sc.nextInt();
+		
 		double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 		System.out.println("length of the line one is: " + length1);
-
-     
-		int a1 = 6;
-		int b1 = 9;
-		int a2 = 10;
-		int b2 = 12;
+		
+		
+		System.out.println("--------------");
+                
+		
+		System.out.println("enter A1 co-ordinate");
+		int a1 = sc.nextInt();
+		System.out.println("enter B1 co-ordinate");
+		int b1 = sc.nextInt();
+		System.out.println("enter A2 co-ordinate");
+		int a2 = sc.nextInt();
+		System.out.println("enter B2 co-ordinate");
+		int b2 = sc.nextInt();
+		
 		double length2 = Math.sqrt((a2-a1)*(a2-a1) + (b2-b1)*(b2-b1));
 		System.out.println("length of line two is: "+length2);
 		
+		
 		String str1 = Double.toString(length1);
     	String str2 = Double.toString(length2);
-    	
-    	boolean eq = str1.equals(str2);
-    	if(eq==true) {
-    		System.out.println("Line has equal length");
-    	}
-    	else {
-    		System.out.println("Line not matched");
-    	}
+    	        
+    	        
+		int res= str1.compareTo(str2);
+		if(res > 0)
+			System.out.println("Line one is greater than line two");
+		else if(res < 0)
+			System.out.println("Line two is greater than line one");
+		else
+			System.out.println("Both lines are equal");
+	
 	}
 
 }
